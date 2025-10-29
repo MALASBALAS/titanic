a364e3c2a84210fa828caeaf3e9f83c17610b3e8
MALASBALAS <alvarobalas19@gmail.com>
2025-10-29 10:20:15 -0500

    Add initial implementation of Botes and ServiciosEmergencia classes with Maven configuration

---
+++ src/main/java/com/alvaroyraul/Botes.java
@@ -0,0 +1,54 @@
+package com.alvaroyraul;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data @AllArgsConstructor
+public class Botes {
+
+    //TODO: El tiempo de demora que cada bote tarda en contar a los pasajeros será aleatorio y va 
+    // de los 2 a los 6 segundos . Thread.sleep(2000); //Demora un proceso 2 segundos
+    private static final int MAXSLEEP = 6000;
+    private static final int MINSLEEP  = 2000;
+
+    //TODO: Hay 20 botes, cada bote tendrá un identificador que irá de B00 a B19.
+    public static final int TOTALBOTES = 20;
+
+    //TODO: Las personas saltaban a los botes y los liberaban para llegar al oceano, 
+    // hasta pasado un tiempo, no se hacía el recuento de personas que viajaban en el bote. 
+    // Podían ir de 1 a 100 personas. En nuestro simulador, cada bote se gestiona de forma autónoma 
+    // generará un número aleatorio de personas.
+    private static final int MAXPERSONAS = 100;
+    private int personas;
+    private int mujeres;
+    private int hombres;
+    private int ninios;
+    private String nbote;
+
+    // Lombok generará el constructor completo con @AllArgsConstructor
+
+    // Metodo para simular la carga de un único bote y devolver sus datos
+    public static Botes cargarBote(int i) {
+        int numPersonas = (int) (Math.random() * MAXPERSONAS) + 1;
+        int mujeres = (int) (Math.random() * (numPersonas + 1));
+        int hombres = (int) (Math.random() * (numPersonas - mujeres + 1));
+        int ninios = numPersonas - mujeres - hombres;
+
+        String nbote = String.format("B%02d", i);
+
+        // crear el objeto Botes con los datos generados
+        Botes bote = new Botes(numPersonas, mujeres, hombres, ninios, nbote);
+
+        // opcional: simular una demora corta (no bloqueante larga)
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        return bote;
+    }
+
+    // se lo deberá comunicar al servicio de Emergencias.
+}
+
---
+++ src/main/java/com/alvaroyraul/ServiciosEmergencia.java
@@ -0,0 +1,33 @@
+package com.alvaroyraul;
+
+// Servicio de Emergencias: invoca Botes, recibe datos y los muestra
+public class ServiciosEmergencia {
+	public static void main(String[] args) {
+
+		int maxpersonas = 0;
+		int maxmujeres = 0;
+		int maxhombres = 0;
+		int maxninios = 0;
+
+		System.out.println("Iniciando servicio de emergencias. Generando datos de botes...");
+
+		for (int i = 0; i < Botes.TOTALBOTES; i++) {
+			Botes bote = Botes.cargarBote(i);
+			// usar toString generado por Lombok @Data
+			System.out.println(bote);
+			// contar totales
+			maxpersonas += bote.getPersonas();
+			maxmujeres += bote.getMujeres();
+			maxhombres += bote.getHombres();
+			maxninios += bote.getNinios();
+		}
+
+		
+
+		System.out.println("Proceso finalizado.");
+		System.out.println("Total personas rescatadas: " + maxpersonas);
+		System.out.println("Total mujeres rescatadas: " + maxmujeres);
+		System.out.println("Total hombres rescatadas: " + maxhombres);
+		System.out.println("Total niños rescatadas: " + maxninios);
+	}
+}
